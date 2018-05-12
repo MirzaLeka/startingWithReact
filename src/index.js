be185b6fe1d0dc00e1bcf6455975c9bdcc235261
Mirza Leka <mirza_376@hotmail.com>
2018-05-12 15:53:51 +0200

    React AJAX & Props

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {App, Sub} from './components/App';
-//import searchBar from "./components/search_bar";
-//ReactDOM.render(<searchBar/>, document.getElementById("searchbar"));
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
-// Renders one component (searchbar) inside another
-const Inp = () => {
-    return (
-    <div>
-    <SearchBar /> {/* instance of a class SearchBar */}
-    </div>
-       );
-    }
+const API_KEY = "AIzaSyAyKU6wP8ZmOhzugkW3ocRssocX8YQ9Qco";
 
-ReactDOM.render(<Inp/>, document.getElementById("searchbar"));
+// In React our main component (in this case index.js) should be the only one responsible for AJAX and thus all other scripts will derive from it
+
+    class AppIndex extends Component  {
+        constructor (props) {
+            super(props);
+
+            this.state = { videos: [] };
+
+            // When component (page) loads, constructor will run right away and that will kick of the search
+            // and then callback (videos) function will be called and it will update our state with the list of videos
+            YTSearch({
+                key: API_KEY,
+                term: 'surfboards'}, (videos) => {
+                this.setState({ videos }); //API data appended with videos => videos : videos (same name)
+                }
+                );
+            }
+            
+         
+
+
+        render() {
+            return (
+                <div>
+                <SearchBar /> 
+                <VideoList videos={this.state.videos}/>
+                </div>
+                )
+        }
+
+    } 
+
+ReactDOM.render(<AppIndex/>, document.getElementById("searchbar"));
 
 ReactDOM.render(<div>
 <App />
@@ -23,3 +48,13 @@ ReactDOM.render(<div>
 </div>  , document.getElementById('app'));
 
 ReactDOM.render(<Sub />, document.querySelector('.sub'));
+
+// Renders one component (searchbar) inside another
+// Functional components can render class components
+
+//<SearchBar /> {/* instance of a class SearchBar */}
+
+// We'll turn functional component Inp into class component because it will have multiple purposes like searching videos
+
+//import searchBar from "./components/search_bar";
+//ReactDOM.render(<searchBar/>, document.getElementById("searchbar"));
